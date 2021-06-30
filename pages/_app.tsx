4933675200006a8a0648b424785b1fdc7d8622ef
hiima <h.iima@buysell-technologies.com>
2021-06-30 22:40:29 +0900

    fix: remove SafetyHydra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,7 @@
 import { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  const SafeHydrate = dynamic(() => import('../components/SafeHydrate'), {
-    ssr: false,
-  });
-
-  return (
-    <SafeHydrate>
-      <Component {...pageProps} />
-    </SafeHydrate>
-  );
-};
+const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
 
 export default App;
