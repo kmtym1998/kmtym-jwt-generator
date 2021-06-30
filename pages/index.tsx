12ccc0c267c6d4931f0a21cd951b56f85a079708
hiima <h.iima@buysell-technologies.com>
2021-06-30 22:39:43 +0900

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useState, useMemo } from 'react';
-import createAuth0Client from '@auth0/auth0-spa-js';
+import { useState, useEffect } from 'react';
+import createAuth0Client, { Auth0Client, User } from '@auth0/auth0-spa-js';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,57 +9,60 @@ import { notify } from '../helpers/notify';
 const Page = () => {
   const AUTH0_PARAM = { audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE };
 
-  const [auth0, setAuth0] = useState<any>();
-  const [profile, setProfile] = useState<Object>();
+  const [auth0Client, setAuth0Client] = useState<Auth0Client | null>(null);
+  const [profile, setProfile] = useState<User>();
   const [idToken, setIdToken] = useState<string>();
 
-  const login = useCallback(async () => {
-    if (!auth0) return;
-
-    await auth0.loginWithPopup(AUTH0_PARAM);
-
-    try {
-      const idTokenClaims = await auth0.getIdTokenClaims(AUTH0_PARAM);
-      setIdToken(idTokenClaims.__raw);
-
-      const user = await auth0.getUser(AUTH0_PARAM);
-      setProfile(user);
-    } catch (e) {
-      console.log(e);
-    }
-  }, [auth0]);
-
-  const logout = useCallback(() => {
-    auth0.logout({
-      returnTo: process.env.NEXT_PUBLIC_AUTH0_LOGOUT_URI,
-    });
-  }, [auth0]);
-
-  useMemo(async () => {
-    if (!auth0) return;
-
-    try {
-      const idTokenClaims = await auth0.getIdTokenClaims(AUTH0_PARAM);
-      setIdToken(idTokenClaims.__raw);
-
-      const user = await auth0.getUser(AUTH0_PARAM);
-      setProfile(user);
-    } catch (e) {
-      console.log(e);
-    }
-  }, [auth0]);
-
-  if (!auth0) {
+  if (!auth0Client) {
     createAuth0Client({
       domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN || '',
       client_id: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || '',
       redirect_uri: process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI,
       audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
       useRefreshTokens: true,
-    }).then(setAuth0);
+      cacheLocation: 'localstorage',
+    }).then(setAuth0Client);
 
     return null;
   }
+
+  const login = async () => {
+    if (!auth0Client) return;
+
+    await auth0Client.loginWithPopup(AUTH0_PARAM);
+
+    try {
+      const idTokenClaims = await auth0Client.getIdTokenClaims(AUTH0_PARAM);
+      setIdToken(idTokenClaims.__raw);
+
+      const user = await auth0Client.getUser(AUTH0_PARAM);
+      setProfile(user);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const logout = () => {
+    auth0Client.logout({
+      returnTo: process.env.NEXT_PUBLIC_AUTH0_LOGOUT_URI,
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (!auth0Client) return;
+
+      try {
+        const idTokenClaims = await auth0Client.getIdTokenClaims(AUTH0_PARAM);
+        setIdToken(idTokenClaims.__raw);
+
+        const user = await auth0Client.getUser(AUTH0_PARAM);
+        setProfile(user);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [auth0Client]);
 
   return (
     <>
