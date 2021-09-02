ade6b6995226edc2621ac736db4de87e8cf0f0d7
kmtym1998 <kmtym.devloper@gmail.com>
2021-09-02 23:13:20 +0900

    feat: idTokenとaccessTokenをそれぞれ取得できるように

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,71 @@
 import Head from 'next/head';
 import { useCallback, useState, useMemo } from 'react';
-import createAuth0Client from '@auth0/auth0-spa-js';
+import { useAuth0 } from '@auth0/auth0-react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import jwtDecode from 'jwt-decode';
 
 import { notify } from '../helpers/notify';
 
 const Page = () => {
-  const AUTH0_PARAM = { audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE };
+  // auth0を呼ぶときのパラメータ
+  const AUTH0_PARAM = {
+    audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
+    scope: 'read:current_user',
+  };
 
-  const [auth0, setAuth0] = useState<any>();
-  const [profile, setProfile] = useState<Object>();
+  // ステートの初期化？
+  const {
+    getAccessTokenWithPopup,
+    getIdTokenClaims,
+    logout: auth0Logout,
+  } = useAuth0();
   const [idToken, setIdToken] = useState<string>();
+  const [profile, setProfile] = useState<Object>(); // idTokenをパースしたやつ
+  const [accessToken, setAccessToken] = useState<string>();
+  const [accessInfo, setAccessInfo] = useState<Object>(); // accessTokenをパースしたやつ
+
+  // idToken取得〜パースするまでの処理まとめたやつ
+  const setUpIdToken = async () => {
+    const idTokenClaims = await getIdTokenClaims(AUTH0_PARAM);
+    const jwt = idTokenClaims.__raw;
+    setIdToken(jwt);
+    const decoded = jwtDecode(jwt);
+    if (typeof decoded === 'object') setProfile(decoded || {});
+  };
+
+  // accessToken取得〜パースするまでの処理まとめたやつ
+  const setUpAccessToken = async () => {
+    const resp = await getAccessTokenWithPopup(AUTH0_PARAM);
+    setAccessToken(resp);
+    const decoded = jwtDecode(resp);
+    if (typeof decoded === 'object') setAccessInfo(decoded || {});
+  };
 
   const login = useCallback(async () => {
-    if (!auth0) return;
-
-    await auth0.loginWithPopup(AUTH0_PARAM);
-
+    await setUpAccessToken();
     try {
-      const idTokenClaims = await auth0.getIdTokenClaims(AUTH0_PARAM);
-      setIdToken(idTokenClaims.__raw);
-
-      const user = await auth0.getUser(AUTH0_PARAM);
-      setProfile(user);
+      await setUpIdToken();
     } catch (e) {
       console.log(e);
     }
-  }, [auth0]);
+  }, []);
 
   const logout = useCallback(() => {
-    auth0.logout({
+    auth0Logout({
       returnTo: process.env.NEXT_PUBLIC_AUTH0_LOGOUT_URI,
     });
-  }, [auth0]);
+  }, []);
 
+  // useMemoってなに？
   useMemo(async () => {
-    if (!auth0) return;
-
     try {
-      const idTokenClaims = await auth0.getIdTokenClaims(AUTH0_PARAM);
-      setIdToken(idTokenClaims.__raw);
-
-      const user = await auth0.getUser(AUTH0_PARAM);
-      setProfile(user);
+      await setUpIdToken();
     } catch (e) {
       console.log(e);
     }
-  }, [auth0]);
-
-  if (!auth0) {
-    createAuth0Client({
-      domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN || '',
-      client_id: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || '',
-      redirect_uri: process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI,
-      audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
-      useRefreshTokens: true,
-    }).then(setAuth0);
-
-    return null;
-  }
+  }, []);
 
   return (
     <>
@@ -75,7 +80,11 @@ const Page = () => {
       )}
       {profile && (
         <>
-          Decoded JWT
+          <button onClick={logout}>LOG OUT</button>
+          <br />
+          <hr />
+          <br />
+          Decoded idToken
           <pre>{JSON.stringify(profile, null, 4)}</pre>
           <div>
             <CopyToClipboard text={`Bearer ${idToken}`} onCopy={notify}>
@@ -83,7 +92,17 @@ const Page = () => {
             </CopyToClipboard>
           </div>
           <ToastContainer />
-          <button onClick={logout}>LOG OUT</button>
+          <br />
+          <hr />
+          <br />
+          Decoded accessToken
+          <pre>{JSON.stringify(accessInfo, null, 4)}</pre>
+          <div>
+            <CopyToClipboard text={`Bearer ${accessToken}`} onCopy={notify}>
+              <button>Copy JWT as bearer token</button>
+            </CopyToClipboard>
+          </div>
+          <ToastContainer />
         </>
       )}
     </>
