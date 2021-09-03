d527c4b064cb0e22d0e177677456d7b70e50a9fd
kmtym1998 <kmtym.devloper@gmail.com>
2021-09-03 11:39:47 +0900

    feat: metadata が空のときはプライベートクレームにメッセージを詰めて返す

--- auth0/rules/private-claim-creator.js
+++ auth0/rules/private-claim-creator.js
@@ -2,13 +2,48 @@
 
 // 実際は関数名いらない
 function redundantDefinition(user, context, callback) {
-  // user_metadataをidTokenのプライベートクレームに突っ込む
-  context.idToken['https://kmtym-jwt-generator.vercel.app'] =
-    user.user_metadata;
+  const PRV_CLAIM_KEY = 'https://kmtym-jwt-generator.vercel.app';
+  const EMOJIs = [
+    '😩',
+    '🦩',
+    '💞',
+    '😇',
+    '🥀',
+    '👽',
+    '🐋',
+    '🌃',
+    '🎯',
+    '🥗',
+    '🔪',
+    '🍣',
+    '🚗',
+    '⛄',
+    '🍄',
+  ];
+  const randEmoji = EMOJIs[new Date().getSeconds() % EMOJIs.length];
 
-  // app_metadataをaccessTokenのプライベートクレームに突っ込む
-  context.accessToken['https://kmtym-jwt-generator.vercel.app'] =
-    user.app_metadata;
+  const { user_metadata, app_metadata } = user;
+  if (!user_metadata) {
+    const msg = `your user_data is not specified ${randEmoji}`;
+    context.idToken[PRV_CLAIM_KEY] = { msg };
+  } else if (Object.keys(user_metadata).length !== 0) {
+    // user_metadataをidTokenのプライベートクレームに突っ込む
+    context.idToken[PRV_CLAIM_KEY] = user_metadata;
+  } else {
+    const msg = `your user_data is not specified ${randEmoji}`;
+    context.idToken[PRV_CLAIM_KEY] = { msg };
+  }
+
+  if (!app_metadata) {
+    const msg = `your app_metadata is not specified ${randEmoji}`;
+    context.accessToken[PRV_CLAIM_KEY] = { msg };
+  } else if (Object.keys(app_metadata).length !== 0) {
+    // app_metadataをaccessTokenのプライベートクレームに突っ込む
+    context.accessToken[PRV_CLAIM_KEY] = app_metadata;
+  } else {
+    const msg = `your app_metadata is not specified ${randEmoji}`;
+    context.accessToken[PRV_CLAIM_KEY] = { msg };
+  }
 
   return callback(null, user, context);
 }
